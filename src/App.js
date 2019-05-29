2b1924b689a5453a185fd1b5e267d75a7f7472e3
Vanessa Henderson <bookvanny@gmail.com>
2019-05-28 20:35:43 -0400

    some recipe info renders

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
+import Recipe from "./Recipe";
 import "./App.css";
+import { async } from "q";
 
 const App = () => {
   const APP_ID = "261751d0";
   const APP_KEY = "b711216cfa940b5549a3935a47616e5d";
 
+  const [recipes, setRecipes] = useState([]);
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+  };
+
   return (
     <div className="App">
-      <h1>Hello!</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+      {recipes.map(recipe => (
+        <Recipe
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
+        />
+      ))}
     </div>
   );
 };
